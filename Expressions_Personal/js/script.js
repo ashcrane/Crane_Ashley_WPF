73e39a2400294e256ac2d976a6123152846b6713
Ashley Crane <Ashley@Ashleys-MacBook-Pro.local>
2013-11-07 19:03:11 -0500

    Update to Assignment Expressions_Personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,2 +1,16 @@
 // Ashley Crane 11/6/2013 Expressions:Personal
 
+//I am finding the average of my final grades in the classes I have taken so far. The formula for the average is the sum of the grades divided by the number of classes I have taken so far.
+
+var digLit = prompt("Please enter your Digital Literacy final grade."); // This will be the final grade of Digital Literacy.
+
+var pyp = prompt("Please enter your Psychology of Play final grade.");  // This will be the final grade of Psychology of Play.
+
+var omdt = prompt("Please enter your Overview of Media Design and Technology."); //This will be the final grade for Overview of Media Design and Technology.
+
+var result = (Number(digLit) + Number(pyp) + Number(omdt))/3; // The grades are now added together and then divided by the number 3.
+
+
+
+console.log(result);
+alert("The overall average of the classes you have taken is " +  result)
