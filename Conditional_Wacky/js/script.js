eb2781d10bec0b05f64ac209dd334756d73fd402
Ashley Crane <Ashley@Ashleys-MacBook-Pro.local>
2013-11-14 18:36:35 -0500

    Added assignment Conditional-Wacky

---
+++ Conditional_Wacky/js/script.js
@@ -0,0 +1,16 @@
+// Ashley Crane  11/14/13  Conditional-Wacky
+
+
+// What will you do if we get some wacky weather?
+
+
+var iceCreamRain = true;  // variable for if it was raining ice cream.
+
+var sodaRain = true;  // variable for if it was raining soda.
+
+
+if(iceCreamRain == true || sodaRain === true){
+    console.log("Bring out cups, bowls and ice cream toppings! Root beer float anyone?"); // If the weather is wacky, will print to console "Bring out cups, bowls and ice cream toppings! Root beer float anyone?"
+}else{
+    console.log("Bummer, looks like regular weather."); // If the weather isn't wacky, will print to console "Bummer, looks like regular weather."
+}
