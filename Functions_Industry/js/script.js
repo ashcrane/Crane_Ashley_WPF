cdf84a9887389888a4ce1f45695b218d20fc23e2
Ashley Crane <Ashley@Ashleys-MacBook-Pro.local>
2013-11-21 16:02:51 -0500

    added comments to the code.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -11,14 +11,16 @@ function tonerPercentage(){   // The function for the toner percentage.
 var percentage = amtOfPages / amtOfYield * 100; // The variable for the percentage of toner used.
 
     console.log("You have used " + percentage + " percent of the toner.");  // Print to console the amount of toner used.
-    if(percentage > 75){
-        console.log("Time for a new toner cartridge!");
-    }else if(percentage == 50){
-        console.log("You are half way through your toner.")
-    }else{
-        console.log("Happy copying!")
-    }
 
+
+    if(percentage >= 75){                                   // If the toner used is greater than or equal to 75 percent then the console will print the need to change toner cartridge.
+        console.log("Time for a new toner cartridge!");
+    }else if(percentage == 50){                            // If the percentage is equal to 50 percent then the console will print that you are halfway through the cartridge.
+        console.log("You are half way through your toner.");
+    }else{                                  // If the percentage is less than 50 percent then console will print that you have enough toner to continue making copies.
+        console.log("Happy copying!");
+    }
+    return percentage;
 }
 
 tonerPercentage();
